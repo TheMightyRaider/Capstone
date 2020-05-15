2aabf134bfa563d8a03f576855f495a26026b780
TheMightyRaider <sivasanjay7@gmail.com>
2020-05-15 16:34:02 +0530

    Basic Chat application using django channels

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/sanjay/GitHub/Channels/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ mysite/mysite/routing.py
@@ -0,0 +1,12 @@
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import chat.routing
+
+application = ProtocolTypeRouter({
+    # (http->django views is added by default)
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+})
