270f980e3a93e7a493432136642b2c3cc0d5e23c
TheMightyRaider <sivasanjay7@gmail.com>
2020-05-22 18:43:07 +0530

    Added LiveStream Functionality

--- chat/routing.py
+++ chat/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/livestream/', consumers.ChatConsumer),
+    re_path(r'ws/livestream/', consumers.LiveStreamConsumer),
 ]
